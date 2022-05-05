37040443a4f9d2a5e8e597080e80468eabcb836a
Ishant <myuworkacc174@gmail.com>
2022-05-05 14:17:57 +0530

    changed link name

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,7 @@ const About = () => {
   return(
       <div className="container">
     <div className="accordion" id="accordionExample">
-    <h2>About</h2>
+    <h2>About us</h2>
     <div className="accordion-item">
       <h2 className="accordion-header" id="headingOne">
         <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
